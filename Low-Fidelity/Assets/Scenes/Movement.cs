0ec666e3d675274b9c373e2d15add87f53694118
LJRReyniers <93527848+LJRReyniers@users.noreply.github.com>
2023-10-13 10:46:42 +0200

    Changed third person to first person

--- Low-Fidelity/Assets/Scenes/Movement.cs
+++ Low-Fidelity/Assets/Scenes/Movement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public Transform cam;
+
     [SerializeField] float moveSpeed = 5f;
     // Start is called before the first frame update
     void Start()
